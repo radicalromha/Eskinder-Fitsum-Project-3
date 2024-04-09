6a512d89d2dbb689748ae98fb3c196d62bbf6093
Eskinder Romha Fitsim <romha223@gmail.com>
2024-04-09 17:24:18 -0500

    corrected coordinates

--- js/map-script.js
+++ js/map-script.js
@@ -1,24 +1,24 @@
 let map;
 
 async function initMap() {
-  // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
+  // The location of Addis Ababa, Ethiopia
+  const position = { lat: -9.0192, lng: 38.7525 };
   // Request needed libraries.
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
+  // The map, centered at Addis Ababa
   map = new Map(document.getElementById("map"), {
     zoom: 4,
     center: position,
     mapId: "DEMO_MAP_ID",
   });
 
-  // The marker, positioned at Uluru
+  // The marker, positioned at Addis Ababa
   const marker = new AdvancedMarkerElement({
     map: map,
     position: position,
-    title: "Uluru",
+    title: "Home",
   });
 }
 
