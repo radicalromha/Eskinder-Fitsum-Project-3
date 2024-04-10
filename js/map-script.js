a0856c0b220b7c1d040d4396ae2193896885bbc1
Eskinder Romha Fitsim <romha223@gmail.com>
2024-04-09 19:07:04 -0500

    corrected script

--- js/map-script.js
+++ js/map-script.js
@@ -1,25 +1,5 @@
-let map;
-
-async function initMap() {
-  // The location of Addis Ababa, Ethiopia
-  const position = { lat: -9.0192, lng: 38.7525 };
-  // Request needed libraries.
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-  // The map, centered at Addis Ababa
-  map = new Map(document.getElementById("map"), {
-    zoom: 4,
-    center: position,
-    mapId: "DEMO_MAP_ID",
-  });
-
-  // The marker, positioned at Addis Ababa
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: "Home",
-  });
+function init() {
+  alert("it works");
 }
 
-initMap();
+google.maps.event.addListener(window, "load", init);
