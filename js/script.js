5b5ee96da197c0f0d31c9270f5aa8e5d66b295be
Eskinder Romha Fitsim <romha223@gmail.com>
2024-04-11 21:45:32 -0500

    added image slider

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
-const toggleButtons = document.querySelectorAll(".toggle-button");
+let slideIndex = 0;
+showSlides();
 
-toggleButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const content = button.nextElementSibling;
-    content.style.display =
-      content.style.display === "none" || content.style.display === ""
-        ? "block"
-        : "none";
-  });
-});
+function showSlides() {
+  let i;
+  const slides = document.getElementsByClassName("slider-image");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
